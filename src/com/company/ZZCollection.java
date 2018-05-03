96044afa6927b5dc511f5d5ca6bb404334fd2727
gzanet <znt.gianluca@gmail.com>
2018-05-03 17:03:16 +0200

    interface List, Class LinkedList

--- src/com/company/ZZCollection.java
+++ src/com/company/ZZCollection.java
@@ -1,9 +1,12 @@
 package com.company;
 
 import com.company.ZZExceptions.ZZEmptyContainerException;
+import com.company.ZZExceptions.ZZInvalidArgumentException;
 import com.company.ZZExceptions.ZZNoAvailableSpaceException;
 import com.company.ZZFunctions.ZZFunction;
 import com.company.ZZFunctions.ZZTest;
+
+import java.util.Iterator;
 
 public interface ZZCollection<T> extends ZZIterable<T> {
 
@@ -11,24 +14,28 @@ public interface ZZCollection<T> extends ZZIterable<T> {
 
     boolean isEmpty();
 
-    T remove() throws ZZEmptyContainerException;
-
-    default void removeAll(){
-        while( !isEmpty() ) {
-            remove();
+    //T remove(Object o) throws ZZEmptyContainerException;
+    /*default void removeAll(){
+        Iterator<T> it = (Iterator<T>) getIterator();
+        while( it.hasNext() ) {
+            remove(it.next());
         }
-    }
-
-    void removeAllExcept(ZZTest<T> tester);
+    }*/
+    /*default void removeAllExcept(ZZIterable<T> collection, ZZTest<T> tester) throws ZZInvalidArgumentException {
+        Iterator<T> it = (Iterator<T>) this.getIterator();
+        while(it.hasNext()){
+            T e = it.next();
+            if( tester.test(e) )
+                remove(e);
+        }
+    }*/
 
     void add(T e) throws ZZNoAvailableSpaceException;
-
     default void addAll(ZZIterable<T> collection) throws ZZNoAvailableSpaceException{
         ZZIterator<T> it = getIterator();
         while( it.hasNext() )
             add( it.getNext() );
     }
-
     default void addAllExcept(ZZIterable<T> collection, ZZTest<T> tester)throws ZZNoAvailableSpaceException{
         ZZIterator<T> it = getIterator();
         while( it.hasNext() ) {
@@ -38,7 +45,5 @@ public interface ZZCollection<T> extends ZZIterable<T> {
             }
         }
     }
-
-    <E> ZZCollection<E> map(ZZFunction<T, E> function);
 
 }
