21c3eb2de972e1ff14b5e8a2a4601add593e265a
filippo <filippo.zanatta@gmail.com>
2018-05-06 11:27:30 +0200

    Correzioni

--- src/com/company/Interfacce/ZZCollection.java
+++ src/com/company/Interfacce/ZZCollection.java
@@ -26,16 +26,16 @@ public interface ZZCollection<T> extends ZZIterable<T> {
     void removeAllExcept(ZZTest<T> tester);
 
     void add(T e) throws ZZNoAvailableSpaceException;
+
     default void addAll(ZZIterable<T> collection) throws ZZNoAvailableSpaceException{
-        ZZIterator<T> it = getIterator();
+        ZZIterator<T> it = collection.getIterator();
         while( it.hasNext() )
             add( it.getNext() );
     }
     default void addAllExcept(ZZIterable<T> collection, ZZTest<T> tester)throws ZZNoAvailableSpaceException{
-        ZZIterator<T> it = getIterator();
+        ZZIterator<T> it = collection.getIterator();
         while( it.hasNext() ) {
-            T elem = it.getNext();
-            if (tester.test(elem)) {
+            T elem = it.getNext();if (tester.test(elem)) {
                 add(elem);
             }
         }
